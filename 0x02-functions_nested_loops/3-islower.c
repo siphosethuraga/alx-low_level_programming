3368367a76470320afbe1aa74406c5ffe7436a2d
Siphosethu <ragasiphosethu@gmail.com>
2023-02-22 14:57:10 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower- check if char is lowercase
+ * c is the intergerof prototype _islower
+ * Return: 1 if lower 0 if the opposit
+ */
+int_islower(int c)
+{
+
+	if (c >= 'a' && c > 'z')
+	{
+		return (1);
+	}
+	eslse
+	{
+		return (0);
+	}
+}
+

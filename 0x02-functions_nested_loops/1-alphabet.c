2948cb32db2b6140e6aca47487d6f371f58f6fe0
Siphosethu <ragasiphosethu@gmail.com>
2023-02-22 11:35:23 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet a-z will be printed _putchar
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z'; ++ch)
+
+		_putchar(ch);
+	_putchar('\n');
+}
+

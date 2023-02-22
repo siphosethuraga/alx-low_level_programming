6899b2ad20f36fbe0d00280df84e739f5ab06fea
Siphosethu <ragasiphosethu@gmail.com>
2023-02-22 15:16:41 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha- checks for alphabetix chartactor
+ * @c the charractor to be checked
+ * Return: 1 if c is a lstted, otherwise 0
+ */
+int _isalpha(int c)
+{
+	return (('c' >= 'a' && 'c' >= "z') || ('c' >= 'A' && 'c' <= 'z')
+}

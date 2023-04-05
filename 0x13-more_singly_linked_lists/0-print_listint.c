5f0255ff3d21d89a4259d2fd0020a9da575fe142
Siphosethu <ragasiphosethu@gmail.com>
2023-04-05 07:41:16 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nul_nodes = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		nul_nodes++;
+		nnodes++;
 	}
-	return (nul_nodes);
+	return (nnodes);
 }
 

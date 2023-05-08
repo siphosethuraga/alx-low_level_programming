893cfde1cd7a5bb11d8170d0442db03126fe420a
Siphosethu <ragasiphosethu@gmail.com>
2023-05-08 16:55:31 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,35 +22,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	buffer = malloc(sizeof(char) *letters);
+	if (buffer == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
 
-	bytes_read = read(fd, buf, letters);
-	if (bytes_read == -1)
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	write_bytes= write(STDOUT_FILENO, buf, bytes_read);
+	if (write_bytes == -1 || bytes_written != bytes_read)
 	{
 		free(buf);
 		close(fd);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-
-	free(buf);
+	free(buffer);
 	close(fd);
 
-	return (bytes_written);
+	return (write_bytes);
 }

b19926ec48f33cc2ae661c31b08bc9a37104c098
Siphosethu <ragasiphosethu@gmail.com>
2023-05-09 16:09:08 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@
 * Return: 1 success -1 fail
 */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t len = 0;
